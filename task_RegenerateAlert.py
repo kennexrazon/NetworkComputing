d1f04426a248915cc782b731836ccd1004d421ac
kennexrazon <kennexrazon@gmail.com>
2017-01-11 09:00:22 +0800

    KHR
    -fixed issue due to the dropping of the script_add column

--- task_RegenerateAlert.py
+++ task_RegenerateAlert.py
@@ -11,32 +11,12 @@ from datetime import timedelta as tda
 from datetime import datetime as dt
 import alertgenDELAYED as ad
 import sys
-#
-#def get_cols():
-#    query = "select name from senslopedb.site_column_props "
-#    query2 = """where name like '____%' """
-#    query = query + query2
-#    return qdb.GetDBDataFrame(query)
-#
-#def write_initial_task(col_name):
-#    timestamp = pd.to_datetime('2016-10-01 00:00:00')
-#    query = """
-#    insert into senslopedb.to_run_scripts(script_name,stat) values ('task_RegenerateAlert.py %s %s','WAITING')
-#    """ %(col_name,str(timestamp))
-#    db,cur = qdb.SenslopeDBConnect('senslopedb')
-#    cur.execute(query)
-#    db.commit()
-#    db.close()
-#    return 1
-#    
-#cols  = get_cols()
-#for i in range(0,len(cols)):
-#    write_initial_task(cols.iloc[i][0])
 
 def write_next_task(col_name,timestamp):
     query = """
-    insert into senslopedb.to_run_scripts(script_name,script_add,stat) values ('task_RegenerateAlert.py %s %s',"",'WAITING')
+    insert into senslopedb.to_run_scripts(script_name,stat) values ('task_RegenerateAlert.py %s %s','WAITING')
     """ %(col_name,str(timestamp))
+#    print query
     db,cur = qdb.SenslopeDBConnect('senslopedb')
     cur.execute(query)
     db.commit()
@@ -50,12 +30,11 @@ def main():
     time = str(sys.argv[3])
     custom_end = pd.to_datetime(date + ' ' + time)
     name = str(name)
-
     try:
         ad.main(name,custom_end)
         next_timestamp = custom_end + tda(minutes=30)
-#        print '\\\\\\\\\\\\\\\\\\\\%s//////////////////////////' %str(next_timestamp)
         if next_timestamp < dt.now():
+            print 'will write next task now'
             write_next_task(name,next_timestamp)
             return 1
         else:
