e1777bccb14ea47ce450142677a6cfd2835ad05e
kennexrazon <kennexrazon@gmail.com>
2017-01-17 09:15:34 +0800

    KHR -changed the query when getting a task -added 'for update' at select query to prevent the dummy_task_overlord.py from querying the same tasks queried by other instances of dummy_task_overlord.py

--- dummy_task_overlord.py
+++ dummy_task_overlord.py
@@ -8,7 +8,7 @@ This scripts gets tasks on queue from the table 'to_run_scripts'.
 import MySQLdb as mysqlDriver
 import os
 import time
-import dummy_config
+import dummy_config as dc
 #import querySenslopeDb.py as qdb
 
 #Hostdb = '127.0.0.1'
@@ -20,15 +20,20 @@ import dummy_config
 
 def get_waiting_task():
     query = """
-    select script_name,task_id from senslopedb.to_run_scripts where stat = 'WAITING' limit 1
-    """    
-    cur.execute(query)
-    entry = cur.fetchall()
+    select script_name,task_id from senslopedb.to_run_scripts where stat = 'WAITING' limit 1 for update
+    """
+    dc.cur.execute(query)
+    entry = dc.cur.fetchall()
     if len(entry) > 0:
         script = entry[0][0]
         task_id = int(entry[0][1])
-        update_stat_running(task_id)
-        return script,task_id
+        while not update_stat_running(task_id): # while not 1 (habang hindi nasusulat)
+            dc.cur.execute(query)   #ulitin ang pagkuha ng tasks
+            entry = dc.cur.fetchall()
+            if len(entry) > 0:
+                script = entry[0][0]
+                task_id = int(entry[0][1])            
+        return script,task_id            
     else:
         print 'No more tasks to do. Quit na.'
         return 0
@@ -37,8 +42,8 @@ def re_run_task(task_id):
     query = """
     select script_name,task_id from senslopedb.to_run_scripts where task_id = %d limit 1
     """ % int(task_id)
-    cur.execute(query)
-    entry = cur.fetchall()
+    dc.cur.execute(query)
+    entry = dc.cur.fetchall()
     script = entry[0][0]
     task_id = int(entry[0][1])
     update_stat_running(task_id)
@@ -50,17 +55,20 @@ def update_stat_running(task_id):
     query = """
     update senslopedb.to_run_scripts set stat = 'RUNNING', worker = '%s', time_start = '%s' where task_id = %d
     """ %(name,str(time_now),task_id)
-    cur.execute(query)
-    db.commit()
-    return 1
+    if dc.cur.execute(query):
+        dc.db.commit()
+        return 1 # assume masususlat
+    else:
+        print 'ERROR in updating to_run_scripts.'
+        return 0
     
 def update_stat_finished(task_id):
     time_now = time.strftime("%Y-%m-%d %H:%M:%S")
     query = """
     update senslopedb.to_run_scripts set stat = 'FINISHED', time_finished = '%s' where task_id = %d
     """ %(str(time_now),task_id)
-    cur.execute(query)
-    db.commit()
+    dc.cur.execute(query)
+    dc.db.commit()
     return 1
 
 def update_stat_error(task_id,desc):
@@ -69,8 +77,8 @@ def update_stat_error(task_id,desc):
     query = """
     update senslopedb.to_run_scripts set stat = 'ERROR%s', time_finished = '%s' where task_id = %d
     """ %(desc,str(time_now),task_id)
-    cur.execute(query)
-    db.commit()
+    dc.cur.execute(query)
+    dc.db.commit()
     return 1
     
 
@@ -78,10 +86,14 @@ def execute(script,task_id):
     cmd = '''python %s''' %(script)    
     try:
         if os.system(cmd) == 0:
+#            print 'os.system(cmd) == 0:'
             update_stat_finished(task_id)
+#            print 'update_stat_finished(task_id)'
             return 1
         else:
+            print 'else'
             update_stat_error(task_id,'cmd')
+            print '''update_stat_error(task_id,'cmd')'''
             return task_id
     except:
         update_stat_error(task_id,'update_stat_error')
@@ -91,9 +103,9 @@ def main():
     while 1:
         script,task_id = get_waiting_task()
         run = execute(script,task_id)
-        if run != 1:
-            script,task_id = re_run_task()
-            run = execute(script,task_id)
+#        if run != 1:
+#            script,task_id = re_run_task()
+#            run = execute(script,task_id)
 
 if __name__ == '__main__':
     main()
